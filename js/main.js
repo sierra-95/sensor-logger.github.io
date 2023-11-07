ef4af9119ab0c8550ee53eb7216383056eb34481
sierra-95 <michaelmachohi@gmail.com>
2023-11-07 16:22:04 +0300

    message

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dashboardButton = document.querySelector('.dashboard-button button');
+    dashboardButton.addEventListener('click', function () {
+        window.location.href = 'https://web-01.holb20233m8xq2.tech/';
+    });
+});
+
